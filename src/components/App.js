3d87c116d2f4720adaa6b9ded6fcc77083dfdca6
nathalia <nathaliatroina@gmail.com>
2022-02-05 21:22:16 -0500

    commit tree e5095ebdbbfca678685d10ff803ea2e9d7b402cf

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,28 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+
+function App(){
+    const [imageDog, setImage] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+    
+        fetch("https://images.dog.ceo/breeds/bulldog-english/mami.jpg")
+        .then((response) => response.json())
+        .then((data) => {
+             setImage(data.message);
+             setIsLoaded(true);
+          });
+      }, []);
+    
+    
+if (!isLoaded) return <p>Loading...</p>;
+
+return <img scr={imageDog} alt="A Random Dog" />;
+
+    }
+
+
+
+export default App
